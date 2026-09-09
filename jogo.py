bb960d632fa7f0ee4b15e45dd6cc029d2fbda5ba
alunoads <alunoads@labfatecaqa.com>
2026-09-09 12:28:59 -0300

    Erro de > corrigido

--- jogo.py
+++ jogo.py
@@ -1,27 +1,30 @@
 import random
 
-print ("Jogo de adivinhação - Você tem 7 tentaivas")
-print("Tente adivinhar o número que estou pensando entre 1 a 100")
+print("Jogo de adivinhação - Você tem 7 tentativas")
+print("Tente adivinhar o número que estou pensando entre 1 e 100")
 
 numero_secreto = random.randint(1, 100)
 contador = 7
 acertou = False
 
-
 while contador > 0:
-    # print(f"Voce tem {contador} tentativas restantes")
-    contador -= 1
+    print(f"Você tem {contador} tentativas restantes")
+
     tentativa = int(input("Digite o seu palpite: "))
+    contador -= 1
+
     if tentativa == numero_secreto:
         print("Número correto!", numero_secreto)
         acertou = True
-        break    
+        break
+
     elif tentativa > numero_secreto:
-        print("Seu número é menor que o número secreto")    
-    else: 
-        print("Seu número é maior que o número secreto")    
+        print("Seu número é maior que o número secreto")
+
+    else:
+        print("Seu número é menor que o número secreto")
 
 if not acertou:
-    print("Você errou. O número secreto era: ", numero_secreto)
+    print("Você errou. O número secreto era:", numero_secreto)
 else:
-    print("Você acertou. Na tentativa: ", 7 - contador + 1)
+    print("Você acertou. Na tentativa:", 7 - contador)
